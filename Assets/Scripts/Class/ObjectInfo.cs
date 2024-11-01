67988621c744845137d5ea2ac0920bcb0e740aea
belfly1111 <belfly1111@naver.com>
2024-11-01 20:27:01 +0900

    Add : ObjectManager
    [변경 사항]
    1. ObjectPoolManager / Spawner 를 만들었습니다.

---
+++ Assets/Scripts/Class/ObjectInfo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class ObjectInfo
+{
+    // 오브젝트 이름
+    public string objectName;
+    // 오브젝트 풀에서 관리할 오브젝트
+    public GameObject perfab;
+    // 몇개를 미리 생성 해놓을건지
+    public int count;
+}
