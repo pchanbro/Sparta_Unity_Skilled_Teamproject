8a617468537260916c5b63bdaacf3262b585329f
belfly1111 <belfly1111@naver.com>
2024-11-06 23:25:13 +0900

    Merge : Merge with Chan
    [변경 사항]
    1. chan님과 merge하였습니다.
    2. 또한 CharacterManager의 접근을 InGameManagers에서 가능하도록 고쳤습니다.

--- Assets/Scripts/Managers/InGameManagers.cs
+++ Assets/Scripts/Managers/InGameManagers.cs
@@ -12,15 +12,18 @@ public class InGameManagers : MonoBehaviour
     [SerializeField] GameManager GameManager;
     [SerializeField] ItemManager ItemManager;
     [SerializeField] ObjectPoolManager ObjectPoolManager;
+    [SerializeField] CharacterManager CharacterManager;
 
     #region Managers
     public static GameManager Game { get { return instance.gameManager; } }
     public static ItemManager Item { get { return instance.itemManager; } }
     public static ObjectPoolManager ObjectPool { get { return instance.objectPoolManager; } }
+    public static CharacterManager Character { get { return instance.characterManager; } }
 
     private GameManager gameManager;
     private ItemManager itemManager;
     private ObjectPoolManager objectPoolManager;
+    private CharacterManager characterManager;
     #endregion
 
     private void Awake()
@@ -37,5 +40,6 @@ public class InGameManagers : MonoBehaviour
         gameManager = GameManager;
         itemManager = ItemManager;
         objectPoolManager = ObjectPoolManager;
+        characterManager = CharacterManager;
     }
 }
